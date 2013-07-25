79d92d48ddf8541590052f596d60cd1f5cc9c884
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-07-25 15:24:08 +0100

    Remove --deployment from bundle install

--- jenkins.sh
+++ jenkins.sh
@@ -2,5 +2,5 @@
 export RAILS_ENV=test
 export DISPLAY=":99"
 
-bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
+bundle install --path "/home/jenkins/bundles/${JOB_NAME}"
 bundle exec rake
