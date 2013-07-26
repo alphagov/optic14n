dfd0bc9a4e0a0458a6ae32d13b7e1f36e16b28b5
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-07-26 10:35:53 +0100

    Remove redundant methods from QueryHash

--- lib/uri/query_hash.rb
+++ lib/uri/query_hash.rb
@@ -2,19 +2,6 @@ module URI
   ##
   # Extends a hash with query string reordering/deleting capabilities
   module QueryHash
-    def ordered_query_string(*args)
-      unmentioned_keys = keys.reject { |key| args.include?(key.to_s) || args.include?(key.to_sym) }
-      (
-        args.uniq.map { |key| render_value(key, self[key]) }.reject { |i| i.nil? } +
-            unmentioned_keys.map { |key| render_value(key, self[key]) }
-      ).join('&')
-    end
-
-    def delete_keys(*args)
-      args.uniq.map { |key| delete(key.to_s) }
-      to_s
-    end
-
     def [](key)
       item = super key
       item = super(key.to_s) if item.nil? || item.length == 0
